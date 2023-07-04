6de8a32052e2384f9ab03b4f6de6e10ec88f3e5d
kamijidias <andrew_kamiji_dias@hotmail.com>
2023-07-04 15:12:16 -0300

    Entendendo o elemento stack de layout

---
+++ stack/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Stack APP'),
+        ),
+        body: Stack(
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.blue,
+            ),
+            Positioned(
+              top: 50,
+              left: 50,
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
