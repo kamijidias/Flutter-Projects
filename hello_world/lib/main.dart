0dc50f0430949f190ef24d935e9a882be04ca529
kamijidias <andrew_kamiji_dias@hotmail.com>
2023-07-04 11:27:30 -0300

    iniciando os estudos com projeto hello world para flutter

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Center(
+        child: Text(
+          'Hello world',
+          style: TextStyle(fontSize: 30, color: Colors.white),
+        ),
+      ),
+    ),
+  );
+}
