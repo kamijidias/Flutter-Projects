6253119347f2e7942a0dd4fbbb865f7ad2d57121
kamijidias <andrew_kamiji_dias@hotmail.com>
2023-07-04 13:29:58 -0300

    Estudando statelessWidget

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text(
-          'Hello world',
-          style: TextStyle(fontSize: 30, color: Colors.white),
+  int value = 10;
+
+  runApp(MyApp(title: 'My first app title', value: value,));
+  value++; // Não consiguimos mudar pois estamos utilizando o StatelessWidget
+
+}
+
+class MyApp extends StatelessWidget {
+  final String title;
+  final int value;
+
+  const MyApp({Key? key, this.title = '', this.value = 0}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(this.title),
+        ),
+        body:  Center(
+          child: Text(
+            'First APP, value is $value',
+            style: const TextStyle(fontSize: 30, color: Colors.black),
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
