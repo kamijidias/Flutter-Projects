9fc040a94c66f0b4b3384b3a07d612efab265dcf
kamijidias <andrew_kamiji_dias@hotmail.com>
2023-07-04 14:45:45 -0300

    Estrutura de rows em flutter

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -19,8 +19,28 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Application Examplo Scaffold'),
         ),
-        body: const Center(
-          child: Text(' My Application'),
+        body: const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Center(
+              child: Text(
+                'My Application',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Center(
+              child: Text(
+                'Same row',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Center(
+              child: Text(
+                'Same row1',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ],
         ),
       ),
     );
