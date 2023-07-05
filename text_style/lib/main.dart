a7840b1baf9c0f75b9135d42a2853b82d520524f
kamijidias <andrew_kamiji_dias@hotmail.com>
2023-07-05 16:43:34 -0300

    Aprendendo sobre os temas em flutter com materialApp

--- text_style/lib/main.dart
+++ text_style/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,21 +9,44 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Text\nText 2',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          fontSize: 50,
-          height: 1.15,
-          fontStyle: FontStyle.italic,
-          fontWeight: FontWeight.w700,
-          color: Colors.black,
-          backgroundColor: Colors.yellow,
-          decoration: TextDecoration.overline,
-          fontFamily: 'Chopin',
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.purple,
+        appBar: AppBar(
+          title: const Text('Application examplo scaffold'),
+          backgroundColor: Colors.purple,
+        ),
+        body: const Center(
+          child: Text(
+            'Text\nText 2',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 50,
+            ),
+          ),
         ),
       ),
     );
   }
-}
+}   
+    
+  //   Center(
+  //     child: Text(
+  //       'Text\nText 2',
+  //       textDirection: TextDirection.ltr,
+  //       style: TextStyle(
+  //         fontSize: 50,
+  //         height: 1.15,
+  //         fontStyle: FontStyle.italic,
+  //         fontWeight: FontWeight.w700,
+  //         color: Colors.black,
+  //         backgroundColor: Colors.yellow,
+  //         decoration: TextDecoration.overline,
+  //         fontFamily: 'Chopin',
+  //       ),
+  //     ),
+  //   );
+  // }
