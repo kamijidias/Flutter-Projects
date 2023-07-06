68affdd852104188925bcbc9426c347024d1add5
kamijidias <andrew_kamiji_dias@hotmail.com>
2023-07-06 16:37:30 -0300

    adicionando os validadores de email e password, para em seguida utilizar na funcao do elevatedButton

--- first_login/lib/login_page.dart
+++ first_login/lib/login_page.dart
@@ -8,11 +8,58 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Login Page'),
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                      label: Text('email'), hintText: 'name@mail.com'),
+                  validator: (email) {
+                    if (email == null || email.isEmpty) {
+                      return 'Digite seu e-mail';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                    controller: _passwordController,
+                    decoration: const InputDecoration(
+                        label: Text('password'),
+                        hintText: 'Enter your password'),
+                    validator: (password) {
+                      if (password == null || password.isEmpty) {
+                        return 'Digite sua senha';
+                      } else if (password.length <= 8) {
+                        return 'Digite uma senha mais forte';
+                      }
+                      return null;
+                    }),
+                const SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {}
+                  },
+                  child: const Text('Login'),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
