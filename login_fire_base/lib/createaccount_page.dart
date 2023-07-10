eb7bc9cab66213c01c24f938b8ea872eae656b2e
kamijidias <andrew_kamiji_dias@hotmail.com>
2023-07-10 09:15:47 -0300

    Criado a estrutura basica para criacao do usuario

--- login_fire_base/lib/createaccount_page.dart
+++ login_fire_base/lib/createaccount_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class CreateAccountPage extends StatefulWidget {
@@ -8,13 +9,44 @@ class CreateAccountPage extends StatefulWidget {
 }
 
 class _CreateAccountPageState extends State<CreateAccountPage> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _firebaseAuth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Create Account Page'),
+        title: Text('Create Account'),
       ),
-      body: Text("Create Account"),
+      body: ListView(
+        children: [
+          TextFormField(
+            controller: _nameController,
+            decoration: InputDecoration(
+              label: Text('Full Name')
+            ),
+          ),
+          TextFormField(
+            controller: _emailController,
+            decoration: InputDecoration(
+              label: Text('E-mail')
+            ),
+          ),
+          TextFormField(
+            controller: _passwordController,
+            decoration: InputDecoration(
+              label: Text('Password')
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+            },
+            child: Text('Create'),
+          ),
+        ],
+      ),
     );
   }
 }
