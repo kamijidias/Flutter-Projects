42b842b866e53d51faba7bf272a7540b72437ce3
kamijidias <andrew_kamiji_dias@hotmail.com>
2023-07-12 14:20:13 -0300

    criado a pagina de create user com integracao com firebase

--- login_fire_base/lib/createaccount_page.dart
+++ login_fire_base/lib/createaccount_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:login_fire_base/auth_page.dart';
 
 class CreateAccountPage extends StatefulWidget {
   const CreateAccountPage({super.key});
@@ -21,32 +22,59 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
         title: Text('Create Account'),
       ),
       body: ListView(
+        padding: EdgeInsets.all(16),
         children: [
           TextFormField(
             controller: _nameController,
-            decoration: InputDecoration(
-              label: Text('Full Name')
-            ),
+            decoration: InputDecoration(label: Text('Full Name')),
           ),
           TextFormField(
             controller: _emailController,
-            decoration: InputDecoration(
-              label: Text('E-mail')
-            ),
+            decoration: InputDecoration(label: Text('E-mail')),
           ),
           TextFormField(
             controller: _passwordController,
-            decoration: InputDecoration(
-              label: Text('Password')
-            ),
+            decoration: InputDecoration(label: Text('Password')),
           ),
           ElevatedButton(
             onPressed: () {
+              createUser();
             },
             child: Text('Create'),
           ),
         ],
       ),
     );
+  }
+
+  createUser() async {
+    try {
+      UserCredential userCredential =
+          await _firebaseAuth.createUserWithEmailAndPassword(
+              email: _emailController.text, password: _passwordController.text);
+      if (userCredential != null) {
+        userCredential.user!.updateDisplayName(_nameController.text);
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => AuthPage()),
+            (route) => false);
+      }
+    } on FirebaseAuthException catch (e) {
+      if(e.code == 'weak-password') {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Weak password'),
+            backgroundColor: Colors.redAccent,
+          ),
+        );
+      } else if (e.code == 'email-already-in-use'){
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Email already in use'),
+            backgroundColor: Colors.redAccent,
+          ),
+        );
+      }
+    }
   }
 }
